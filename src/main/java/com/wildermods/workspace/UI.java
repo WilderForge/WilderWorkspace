7edc6cd019e59516574160891e665bcbe54c431b
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2021-12-24 01:04:33 -0500

    Add non-functional UI

---
+++ src/main/java/com/wildermods/workspace/UI.java
@@ -0,0 +1,535 @@
+package com.wildermods.workspace;
+
+import java.awt.EventQueue;
+import java.io.File;
+import java.nio.file.InvalidPathException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.text.DefaultCaret;
+
+import com.wildermods.workspace.gui.SmartScroller;
+
+import javax.swing.JButton;
+import javax.swing.JSeparator;
+import javax.swing.JCheckBox;
+import javax.swing.JFileChooser;
+
+import java.awt.Color;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.UIManager;
+import javax.swing.JRadioButton;
+import javax.swing.SwingConstants;
+
+public class UI {
+	
+	static final File defaultRootInstallation;
+	static {
+		String os = System.getProperty("os.name").toLowerCase();
+		if(os != null && os.contains("windows")) {
+			defaultRootInstallation = new File(new File(".").toPath().getRoot().toAbsolutePath().toString() + "/Program Files/Steam/steamapps/common/Wildermyth");
+		}
+		else {
+			defaultRootInstallation = new File(System.getProperty("user.home") + "/.local/share/Steam/steamapps/common/Wildermyth");
+		}
+	}
+
+	private JFrame frame;
+	private JTextField txtModdedfiledirectory;
+	private JCheckBox chckbxCopyModsFolder;
+	private JCheckBox chckbxCopySaveData;
+	private JCheckBox forceCopy;
+	private JTextField txtUnmoddedFileDirectory;
+	private JScrollPane scrollPane;
+	private JTextArea textArea;
+	private JCheckBox chckbxCreateGradleWorkspace;
+	//private JRadioButton rdbtnAdvancedDecomposition;
+	private JButton btnCreateNewModded;
+	private SmartScroller xSmartScroller;
+	private SmartScroller ySmartScroller;
+	private JCheckBox chckbxOverwriteModsFolder;
+	private JCheckBox chckbxOverwriteSaves;
+	private JCheckBox chckbxOverwriteCoreGame;
+	private JRadioButton nuke;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UI window = new UI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public UI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setTitle("Wilderforge Installer");
+		frame.setBounds(100, 100, 850, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.setResizable(false);
+		
+		JLabel lblUnmoddedDirectory = new JLabel("Source Directory:");
+		lblUnmoddedDirectory.setToolTipText("The directory of your unmodified wildermyth installation. Usually in your steam folder.");
+		lblUnmoddedDirectory.setBounds(12, 12, 165, 15);
+		frame.getContentPane().add(lblUnmoddedDirectory);
+		
+		txtUnmoddedFileDirectory = new JTextField();
+		txtUnmoddedFileDirectory.setToolTipText("The directory of your unmodified wildermyth installation. Usually in your steam folder.");
+		if(defaultRootInstallation.exists()) {
+			txtUnmoddedFileDirectory.setText(defaultRootInstallation.getAbsolutePath());
+		}
+		txtUnmoddedFileDirectory.setBounds(171, 10, 538, 19);
+		frame.getContentPane().add(txtUnmoddedFileDirectory);
+		txtUnmoddedFileDirectory.setColumns(10);
+		
+		JButton btnChangeUnmodded = new JButton("Change");
+		btnChangeUnmodded.setToolTipText("Change where wilderforge will copy the game files from");
+		btnChangeUnmodded.addActionListener(listener -> {
+			frame.setEnabled(false);
+			JFileChooser fileChooser = new JFileChooser();
+			if(defaultRootInstallation.exists()) {
+				fileChooser.setCurrentDirectory(defaultRootInstallation);
+			}
+			fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			fileChooser.setAcceptAllFileFilterUsed(true);
+			fileChooser.setDialogTitle("Select default game installation");
+			fileChooser.showOpenDialog(frame);
+			File file = fileChooser.getSelectedFile();
+			if(file != null) {
+				txtUnmoddedFileDirectory.setText(file.getAbsolutePath());
+			}
+			else {
+				txtUnmoddedFileDirectory.setText(null);
+			}
+			updateState();
+			frame.setEnabled(true);
+		});
+		btnChangeUnmodded.setBounds(721, 10, 117, 19);
+		frame.getContentPane().add(btnChangeUnmodded);
+		
+		JLabel lblModdedDirectory = new JLabel("Destination Directory:");
+		lblModdedDirectory.setToolTipText("Where your new modded game instance will be located.");
+		lblModdedDirectory.setBounds(12, 37, 165, 15);
+		frame.getContentPane().add(lblModdedDirectory);
+		
+		txtModdedfiledirectory = new JTextField();
+		txtModdedfiledirectory.setToolTipText("Where your new modded game instance will be located.");
+		txtModdedfiledirectory.setBounds(171, 35, 538, 19);
+		frame.getContentPane().add(txtModdedfiledirectory);
+		txtModdedfiledirectory.setColumns(10);
+		
+		JButton btnChangeModded = new JButton("Change");
+		btnChangeModded.setToolTipText("Change where your new Wildermyth game instance will be located.");
+		btnChangeModded.addActionListener(listener -> {
+			frame.setEnabled(false);
+			JFileChooser fileChooser = new JFileChooser();
+			if(defaultRootInstallation.exists()) {
+				fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+			}
+			fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			fileChooser.setAcceptAllFileFilterUsed(true);
+			fileChooser.setDialogTitle("Select default game installation");
+			fileChooser.showOpenDialog(frame);
+			File file = fileChooser.getSelectedFile();
+			if(file != null) {
+				txtModdedfiledirectory.setText(file.getAbsolutePath());
+			}
+			else {
+				txtModdedfiledirectory.setText(null);
+			}
+			updateState();
+			frame.setEnabled(true);
+		});
+		btnChangeModded.setBounds(721, 35, 117, 19);
+		frame.getContentPane().add(btnChangeModded);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(12, 64, 826, 2);
+		frame.getContentPane().add(separator);
+		
+		chckbxCopyModsFolder = new JCheckBox("Copy Mods Folder");
+		chckbxCopyModsFolder.setToolTipText("Copy all files located in the mods folder into your new game instance.");
+		chckbxCopyModsFolder.setBounds(12, 74, 165, 23);
+		frame.getContentPane().add(chckbxCopyModsFolder);
+		
+		chckbxCopySaveData = new JCheckBox("Copy Save Data");
+		chckbxCopySaveData.setToolTipText("Copy all save data and backups into your new game instance.");
+		chckbxCopySaveData.setBounds(12, 99, 165, 23);
+		frame.getContentPane().add(chckbxCopySaveData);
+		
+		JButton btnHelp = new JButton("Help");
+		btnHelp.setBounds(721, 73, 117, 25);
+		frame.getContentPane().add(btnHelp);
+		
+		btnCreateNewModded = new JButton("Create New Game Instance");
+		btnCreateNewModded.setBounds(12, 130, 826, 71);
+		btnCreateNewModded.addActionListener((listener) -> {
+			if (updateState()) {
+				System.out.println("copy shit");
+			}
+		});
+		frame.getContentPane().add(btnCreateNewModded);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(12, 210, 826, 239);
+		frame.getContentPane().add(scrollPane);
+		
+		xSmartScroller = new SmartScroller(scrollPane, SmartScroller.HORIZONTAL, SmartScroller.START);
+		ySmartScroller = new SmartScroller(scrollPane, SmartScroller.VERTICAL, SmartScroller.END);
+		
+		textArea = new JTextArea();
+		scrollPane.setViewportView(textArea);
+		textArea.setTabSize(4);
+		textArea.setEditable(false);
+		DefaultCaret  caret = (DefaultCaret) textArea.getCaret();
+		caret.setUpdatePolicy(DefaultCaret.NEVER_UPDATE);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBounds(16, 475, 822, 2);
+		frame.getContentPane().add(separator_1);
+		
+		JLabel lblforJavaDevelopers = new JLabel("Warning: The settings below are for coremod developers only!");
+		lblforJavaDevelopers.setBackground(Color.GRAY);
+		lblforJavaDevelopers.setForeground(UIManager.getColor("OptionPane.errorDialog.border.background"));
+		lblforJavaDevelopers.setBounds(12, 489, 466, 23);
+		lblforJavaDevelopers.setOpaque(true);
+		frame.getContentPane().add(lblforJavaDevelopers);
+		
+		chckbxCreateGradleWorkspace = new JCheckBox("Create Gradle Decomp Workspace");
+		chckbxCreateGradleWorkspace.setBackground(Color.GRAY);
+		chckbxCreateGradleWorkspace.setToolTipText("Creates a gradle workspace in the destination directory.");
+		chckbxCreateGradleWorkspace.setForeground(UIManager.getColor("OptionPane.errorDialog.border.background"));
+		chckbxCreateGradleWorkspace.setBounds(12, 512, 466, 23);
+		frame.getContentPane().add(chckbxCreateGradleWorkspace);
+		
+		forceCopy = new JCheckBox("Force Copy");
+		forceCopy.setEnabled(false);
+		forceCopy.setBackground(Color.GRAY);
+		forceCopy.setForeground(Color.YELLOW);
+		forceCopy.setBounds(580, 99, 129, 23);
+		frame.getContentPane().add(forceCopy);
+		
+		chckbxOverwriteSaves = new JCheckBox("Overwrite Saves");
+		chckbxOverwriteSaves.setBackground(Color.GRAY);
+		chckbxOverwriteSaves.setForeground(UIManager.getColor("OptionPane.errorDialog.border.background"));
+		chckbxOverwriteSaves.setBounds(183, 99, 156, 23);
+		frame.getContentPane().add(chckbxOverwriteSaves);
+		
+		chckbxOverwriteModsFolder = new JCheckBox("Overwrite Mods");
+		chckbxOverwriteModsFolder.setForeground(UIManager.getColor("OptionPane.errorDialog.border.background"));
+		chckbxOverwriteModsFolder.setBackground(Color.GRAY);
+		chckbxOverwriteModsFolder.setBounds(183, 74, 156, 23);
+		frame.getContentPane().add(chckbxOverwriteModsFolder);
+		
+		chckbxOverwriteCoreGame = new JCheckBox("Overwrite Core Game Files");
+		chckbxOverwriteCoreGame.setForeground(UIManager.getColor("OptionPane.errorDialog.border.background"));
+		chckbxOverwriteCoreGame.setBackground(Color.GRAY);
+		chckbxOverwriteCoreGame.setBounds(343, 74, 231, 23);
+		frame.getContentPane().add(chckbxOverwriteCoreGame);
+		
+		
+		nuke = new JRadioButton("*NUKE Destination Directory*");
+		nuke.setEnabled(false);
+		nuke.setBackground(UIManager.getColor("OptionPane.errorDialog.titlePane.background"));
+		nuke.setForeground(Color.RED);
+		nuke.setHorizontalAlignment(SwingConstants.CENTER);
+		nuke.setBounds(343, 99, 233, 23);
+		frame.getContentPane().add(nuke);
+		
+		
+		/*
+		rdbtnAdvancedDecomposition = new JRadioButton("Advanced Decompilation");
+		rdbtnAdvancedDecomposition.setToolTipText("Custom decomp settings");
+		rdbtnAdvancedDecomposition.setEnabled(false);
+		rdbtnAdvancedDecomposition.setForeground(Color.RED);
+		rdbtnAdvancedDecomposition.setBounds(51, 532, 228, 23);
+		frame.getContentPane().add(rdbtnAdvancedDecomposition);
+		*/
+		
+		updateState();
+		Thread t = new Thread(() -> {
+			try {
+				while(true) {
+					Thread.sleep(1500);
+					EventQueue.invokeLater(() -> updateState());
+				}
+			}
+			catch(InterruptedException e) {}
+		});
+		t.setName("GUI Update Thread");
+		t.setDaemon(true);
+		t.start();
+	}
+	
+	private boolean updateState() {
+		
+		String text = textArea.getText();
+		
+		StringBuilder s = new StringBuilder();
+		String[] errors = checkErrors();
+		String[] warnings = checkWarnings();
+		String[] checkInfo = checkInfo();
+		if(errors.length != 0) {
+			btnCreateNewModded.setEnabled(false);
+			append(s, 0, (errors.length > 1 ? "There are " + errors.length + " errors" : "There is " + errors.length + " error") + " that must resolved before installation can begin:");
+			for(String e : errors) {
+				append(s, 1, e);
+			}
+			s.append('\n');
+		}
+		else {
+			btnCreateNewModded.setEnabled(true);
+		}
+		
+		if(warnings.length != 0) {
+			append(s, 0, (warnings.length > 1 ? "WARNING - there are " + warnings.length + " warnings: " : "WARNING:"));
+			for(String w : warnings) {
+				append(s, 1, w);
+			}
+			s.append('\n');
+		}
+		
+		textArea.setText(s.toString());
+		
+		return errors.length == 0 && text.equals(textArea.getText()); // return false if there is an error or if the ui has updated since the user clicked
+	}
+	
+	private String[] checkErrors() {
+		List<String> errors = new ArrayList<String>();
+		
+		File unmoddedDir = getFile(txtUnmoddedFileDirectory.getText());
+		File newInstanceDir = getFile(txtModdedfiledirectory.getText());
+		boolean invalidSourceDir = false;
+		boolean invalidDestDir = false;
+		
+		if(unmoddedDir == null) {
+			errors.add("* The source directory has not been selected. This is usually * /Steam/steamapps/common/Wildermyth" );
+		}
+		else {
+			try {
+				unmoddedDir.toPath();
+			}
+			catch (InvalidPathException e) {
+				invalidSourceDir = true;
+				errors.add("* The source directory is invalid: " + e.getMessage());
+			}
+			
+			if(newInstanceDir == null) {
+				errors.add("* The destination directory has not been selected.");
+			}
+			else if (newInstanceDir.exists()) {
+				try {
+					newInstanceDir.getPath();
+				}
+				catch(InvalidPathException e) {
+					invalidDestDir = true;
+					errors.add("The destination directory is invalid: " + e.getMessage());
+				}
+				if(!invalidDestDir) {
+					if(!newInstanceDir.canWrite()) {
+						errors.add("* The destination directory is not writable: " + newInstanceDir);
+					}
+					if(!newInstanceDir.canRead()) {
+						errors.add("* The destination directory is not readable: " + newInstanceDir);
+					}
+					if(newInstanceDir.isFile()) {
+						errors.add("* The destination path chosen is a file, not a directory: " + newInstanceDir);
+					}
+				}
+			}
+			
+			if(!invalidSourceDir && !invalidDestDir) {
+				if(!unmoddedDir.exists()) {
+					errors.add("* The source directory does not exist: " + unmoddedDir);
+				}
+				else {
+					if (!unmoddedDir.canRead()) {
+						errors.add("* The source directory is not readable: " + unmoddedDir);
+					}
+					if (unmoddedDir.isFile()) {
+						errors.add("* The source path chosen is a file, not a directory: " + unmoddedDir);
+					}
+					
+					File versionFile = new File(unmoddedDir + "/version.txt");
+					File modsDir = new File(newInstanceDir + "/mods");
+					File savesDir = new File(newInstanceDir + "/players");
+					
+					boolean showForceCopy = false;
+					
+					if(versionFile != null && !versionFile.exists()) {
+						if(!forceCopy.isSelected()) {
+							errors.add("* The source path does not appear to be a Wildermyth installation (No version.txt detected). Confirm that the source path is the root directory of your Wildermyth installation.\n\t\t-If the directory is correct, check 'Force Copy' checkbox above to copy anyway.");
+						}
+						showForceCopy = true;
+					}
+					
+					if(newInstanceDir != null && newInstanceDir.exists() && newInstanceDir.listFiles() != null && newInstanceDir.listFiles().length > 0) {
+						if(!forceCopy.isSelected()) {
+							errors.add("* The destination directory is not empty. Check 'Force Copy' checkbox above to copy anyway.");
+						}
+						showForceCopy = true;
+					}
+					
+					if(showForceCopy) {
+						forceCopy.setEnabled(true);
+						forceCopy.setVisible(true);
+					}
+					else {
+						forceCopy.setSelected(false);
+						forceCopy.setEnabled(false);
+						forceCopy.setVisible(false);
+					}
+					
+					if(modsDir != null && chckbxCopyModsFolder.isSelected() && modsDir.exists() && !nuke.isSelected() && forceCopy.isSelected()) {
+						chckbxOverwriteModsFolder.setEnabled(true);
+						chckbxOverwriteModsFolder.setVisible(true);
+					}
+					else {
+						chckbxOverwriteModsFolder.setEnabled(false);
+						chckbxOverwriteModsFolder.setSelected(false);
+						chckbxOverwriteModsFolder.setVisible(false);
+					}
+					
+					if(savesDir != null && chckbxCopySaveData.isSelected() && savesDir.exists() && !nuke.isSelected() && forceCopy.isSelected()) {
+						chckbxOverwriteSaves.setEnabled(true);
+						chckbxOverwriteSaves.setVisible(true);
+					}
+					else {
+						chckbxOverwriteSaves.setEnabled(false);
+						chckbxOverwriteSaves.setSelected(false);
+						chckbxOverwriteSaves.setVisible(false);
+					}
+					
+				}
+			}
+		}
+		
+		boolean noErrors = errors.size() == 0;
+		
+		chckbxOverwriteCoreGame.setVisible(noErrors && forceCopy.isEnabled());
+		chckbxCopyModsFolder.setEnabled(noErrors);
+		chckbxCopySaveData.setEnabled(noErrors);
+		if(!chckbxOverwriteCoreGame.isVisible() || nuke.isSelected()) {
+			chckbxOverwriteCoreGame.setSelected(false);
+			chckbxOverwriteCoreGame.setVisible(false);
+		}
+		
+		/*
+		if(noErrors && newInstanceDir.exists() && newInstanceDir.listFiles().length > 0) {
+			nuke.setEnabled(true);
+			nuke.setVisible(true);
+		}
+		else {*/
+			nuke.setVisible(false);
+			nuke.setEnabled(false);
+			nuke.setSelected(false);
+		//}
+		
+		
+		return errors.toArray(new String[]{});
+	}
+	
+	private String[] checkWarnings() {
+		List<String> warnings = new ArrayList<String>();
+		
+		File unmoddedDir = getFile(txtUnmoddedFileDirectory.getText());
+		File newInstanceDir = getFile(txtModdedfiledirectory.getText());
+		
+		int unmoddedDirLength = 0;
+		int newInstanceDirLength = 0;
+		
+		try {
+			if(unmoddedDir != null) {
+				unmoddedDirLength = unmoddedDir.toPath().toAbsolutePath().toString().length();
+			}
+			if(newInstanceDir != null) {
+				newInstanceDirLength = newInstanceDir.toPath().toAbsolutePath().toString().length();
+			}
+		}
+		catch(InvalidPathException e) {
+			//swallow
+		}
+		
+		
+		if(nuke.isSelected()) {
+			warnings.add("*** ALL FILES IN " + newInstanceDir + " WILL BE DELETED ***");
+			warnings.add("*** ALL FILES IN " + newInstanceDir + " WILL BE DELETED ***");
+			warnings.add("*** ALL FILES IN " + newInstanceDir + " WILL BE DELETED ***");
+		}
+		
+		
+		if(unmoddedDirLength >= 200) {
+			warnings.add("* Source directory is " + unmoddedDirLength + " characters long... Files might not copy over correctly on windows systems, resulting in an unusable instance.");
+		}
+		else if (newInstanceDirLength >= 200) {
+			warnings.add("* Destination directory is " + newInstanceDirLength + " characters long... Files might not copy over correctly on windows systems, resulting in an unusable instance.");
+		}
+		
+		if(chckbxOverwriteSaves.isSelected()) {
+			warnings.add("* Existing save files in destination directory will be overwritten if they exist in the source directory");
+		}
+		if(chckbxOverwriteCoreGame.isSelected()) {
+			warnings.add("* All core game files (assets & jar files) will be overwritten if they exist in the source directory");
+		}
+		if(chckbxOverwriteModsFolder.isSelected()) {
+			warnings.add("* Existing files in destination mod directory will be overwritten if they exist in the source directory");
+		}
+		
+		if(forceCopy.isSelected()) {
+			if(!new File(unmoddedDir + "/version.txt").exists()) {
+				warnings.add("* Source directory may not be a valid Wildermyth installation. User is force copying anyway.");
+			}
+			if(newInstanceDir.listFiles() != null && newInstanceDir.listFiles().length > 0) {
+				warnings.add("* Destination directory is not empty. User is force copying anyway.");
+			}
+		}
+		
+		if(chckbxCreateGradleWorkspace.isSelected()) {
+			warnings.add("* Installation will include a gradle development environment.");
+		}
+		
+		return warnings.toArray(new String[]{});
+	}
+	
+	private String[] checkInfo() {
+		return new String[0];
+	}
+	
+	private StringBuilder append(StringBuilder b, int indent, String s) {
+		for(int i = 0; i < indent; i++) {
+			b.append('\t');
+		}
+		b.append(s).append('\n');
+		return b;
+	}
+	
+	private File getFile(String path) {
+		if(path != null && !path.isEmpty()) {
+			return new File(path);
+		}
+		return null;
+	}
+}

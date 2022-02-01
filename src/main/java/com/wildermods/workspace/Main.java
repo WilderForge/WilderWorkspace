259793c9ce3f5d00cc3d8698a4cad018c8ffd787
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2022-02-01 16:17:59 -0500

    Documentation

--- src/main/java/com/wildermods/workspace/Main.java
+++ src/main/java/com/wildermods/workspace/Main.java
@@ -4,6 +4,20 @@ import java.lang.reflect.InvocationTargetException;
 
 public class Main {
 	
+	/**
+	 * You should generally just create your UI class yourself using
+	 * args passed to your main function.
+	 * 
+	 * @param args
+	 * @throws InterruptedException
+	 * @throws ClassNotFoundException
+	 * @throws NoSuchMethodException
+	 * @throws SecurityException
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 * @throws IllegalArgumentException
+	 * @throws InvocationTargetException
+	 */
 	@Deprecated
 	public static void main(String[] args) throws InterruptedException, ClassNotFoundException, NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		UI.getUI(args);
